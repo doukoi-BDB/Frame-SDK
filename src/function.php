e4bac508b891a0de8758ecf9111cffe880080030
doukoi-BDB <gtcarry888@163.com>
2022-03-25 11:41:43 +0800

    fix-1

---
+++ src/function.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| BDB-Frame sdk/txz Notice:
+|--------------------------------------------------------------------------
+|
+| The SDK contains functions, classes, APIs, and demos.
+| Copy and paste for use
+| Welcome to use. If you have any questions, please contact the community
+|
+*/
+
+
